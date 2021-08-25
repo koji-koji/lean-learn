385f88baa469e7fb5d58568e4a45577165cd2902
koji koyama <k.koyama37@gmail.com>
2021-08-25 19:20:10 +0900

    feat: SeparateHrんコンポーネントの実装。

---
+++ components/parts/SeparateHr.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SeparateHr: React.FC = () => {
+  return (
+    <>
+      <hr className="my-4 border-t border-gray-300" />
+    </>
+  )
+}
+
+export default SeparateHr
+4
