d545c3fde5154554d5616711034b0a233bada7fe
koji koyama <k.koyama37@gmail.com>
2021-09-07 23:06:49 +0900

    test: 副守備の追加・削除アイコンのテスト項目作成。

--- components/layouts/LearnSettingCardComponent.test.tsx
+++ components/layouts/LearnSettingCardComponent.test.tsx
@@ -8,14 +8,16 @@ import React from 'react'
 import LearnSettingCardComponent from '../mass/LearnSettingCardComponent'
 import userEvent from '@testing-library/user-event'
 
-describe('削除ボタン', () => {
-  test('追加アイコンをクリックすると1つ増える。', () => {
-    render(<LearnSettingCardComponent />)
-    const pre = screen.getAllByText(/学習項目/i)
-    userEvent.click(screen.getByTestId('addLearnInfoIcon'))
-    const after = screen.getAllByText(/学習項目/i)
+describe('学習項目追加・削除', () => {
+  describe('追加アイコン', () => {
+    test('クリックすると学習項目が1つ増える。', () => {
+      render(<LearnSettingCardComponent />)
+      const pre = screen.getAllByText(/学習項目/i)
+      userEvent.click(screen.getByTestId('addLearnInfoIcon'))
+      const after = screen.getAllByText(/学習項目/i)
 
-    expect(after.length - pre.length).toBe(1)
+      expect(after.length - pre.length).toBe(1)
+    })
   })
 
   describe('削除ボタン', () => {
@@ -39,6 +41,22 @@ describe('削除ボタン', () => {
       expect(afterDeleteClickButtons[0].closest('button')).toHaveAttribute(
         'disabled'
       )
+    })
+  })
+})
+
+describe('復習日の追加・削除', () => {
+  describe('追加アイコン', () => {
+    test('クリックすると復習日が1つ増える。', () => {
+      console.log('test')
+    })
+  })
+  describe('削除アイコン', () => {
+    test('1つのときにはdisabledになっている', () => {
+      console.log('test')
+    })
+    test('2つある時にクリックすると、1回目は削除できて、2回目は削除できない。', () => {
+      console.log('test')
     })
   })
 })
